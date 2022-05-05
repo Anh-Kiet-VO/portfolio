a8da881a58c10dd20c9f642c2ab910868d1015b6
Anh-Kiet <anhkietakpro@gmail.com>
2022-05-05 15:58:16 +0200

    headers + social

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+
+const HeaderSocials = () => {
+	return (
+		<div className='headers__socials'>
+			<a rhef="https://linkedin.com" target="_blank"><BsLinkedin /></a>
+			<a rhef="https://github.com" target="_blank"><FaGithub /></a>
+		</div>
+	)
+}
+
+export default HeaderSocials
