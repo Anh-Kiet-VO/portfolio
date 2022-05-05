d54f9ad1db70e5f51798cd5bf8b5237bae68431c
Anh-Kiet <anhkietakpro@gmail.com>
2022-05-05 17:27:13 +0200

    about content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -19,20 +19,30 @@ const About = () => {
 					<div className='about__cards'>
 						<article className='about__card'>
 							<FaAward className='about__icon' />
-							<h5>Experience</h5>
-							<small>13+ Years Experience</small>
+							<h5>Club</h5>
+							<small>13+ Years at Real Madrid at the highest level</small>
 						</article>
 
 						<article className='about__card'>
 							<FiUsers className='about__icon' />
-							<h5>A propos</h5>
-							<small>13+ Years Experience</small>
+							<h5>Trophies</h5>
+							<small>4x Champions League </small>
+							<small>3x La Liga </small>
+							<small>2x Copa Del Rey </small>
+						</article>
+
+						<article className='about__card'>
+							<FiUsers className='about__icon' />
+							<h5>2021-2022 Stats</h5>
+							<small>46 Goals & 14 Decisive pass</small>
 						</article>
 					</div>
-					<p>Le Glock 19 est un pistolet semi-automatique conçu et fabriqué depuis 1988 par l'entreprise Glock pour
-						les forces militaires et les services de police. C'est l'équivalent du Glock 17, en version compacte.
-						Il est considéré comme l'un des meilleurs pistolets semi-automatiques de nos jours, en raison de sa robustesse,
-						de sa fiabilité et de sa simplicité de mise en œuvre
+					<p>
+						Karim Benzema est considéré comme l'un des meilleurs attaquants de sa génération, et l'un des meilleurs joueurs
+						à avoir évolué dans le championnat d'Espagne. Régulièrement nommé pour le Ballon d'or (4e en 2021),
+						Karim Benzema est le meilleur buteur français de l'histoire de la Ligue des champions, le meilleur buteur français
+						de l'histoire toutes compétitions confondues et le meilleur buteur français de l'histoire de la Liga.
+						Il est actuellement le cinquième meilleur buteur de l'histoire de l'équipe de France.
 					</p>
 
 					<a href="#contact" className='btn btn-primary'>Me contacter</a>
